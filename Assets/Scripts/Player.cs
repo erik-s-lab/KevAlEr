feb9b82ead39c1eb1c8d510050618d903ac4d762
erik-s-linux <60352006+erik-s-linux@users.noreply.github.com>
2020-08-23 19:59:00 +0200

    first commit

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private string[] state;
+    private string playerName;
+    private int money;
+    private bool playerNameSettet;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerName = "Nicht Zugewiesen";
+        playerNameSettet = false;
+        for (int i = 0; i >= 2;i++)
+        {
+            state[i] = "mx.s";
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Invert(playerNameSettet))
+        {
+            SetPlayerName();
+        }
+    }
+
+    private void SetPlayerName()
+    {
+
+    }
+
+    private bool Invert(bool a)
+    {
+        return a == false;
+    }
+}
